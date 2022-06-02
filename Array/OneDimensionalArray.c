55bd33a267040a8266b93e704750d072a97bcdaa
Belcheria <741533226@qq.com>
2022-06-02 10:40:44 +0800

    重新规范数组的形式，规定0为初始值也为空值

--- Array/OneDimensionalArray.c
+++ Array/OneDimensionalArray.c
@@ -2,7 +2,7 @@
 
 void InitList(int* oneDimensionalArray)
 {
-	oneDimensionalArray = malloc(sizeof(MaxLength * sizeof(int)));
+	oneDimensionalArray = calloc(sizeof(MaxLength, sizeof(int));
 
 	return;
 }
@@ -26,10 +26,27 @@ int LocateElem(int* oneDimensionalArray, int elemVal, int arraySize)
 	return elemLoc;
 }
 
-int GetElem(int* oneDimensionalArray, int elemLoc);
+int GetElem(int* oneDimensionalArray, int elemLoc, int arraySize)
+{
+	int elemVal = 0;
+
+	if (elemLoc < 0 || elemLoc >= arraySize) {
+		return;
+	}
+
+	elemVal = oneDimensionalArray[elemLoc];
+
+	return elemVal;
+}
 
 
-void InsertElem(int* oneDimensionalArray, int elemLoc, int elemVal);
+void InsertElem(int* oneDimensionalArray, int elemLoc, int elemVal)
+{
+
+
+	return;
+}
+
 void DeleteElem(int* oneDimensionalArray, int elemLoc, int* returnElemVal);
 void PrintList(int* oneDimensionalArray);
 bool IsEmpty(int* oneDimensionalArray);
--- Array/OneDimensionalArray.h
+++ Array/OneDimensionalArray.h
@@ -14,6 +14,15 @@ int Length(int* oneDimensionalArray, int* arraySize);
 等我看看变长数组
 实际上Length可以使用sizeof(array)/sizeof(type)获取，但在函数中oneDimensionalArray作为指针传入，即始终等于8个字节
 */
+
+/*
+----2022/6/2----
+C Primer Plus中的变长数组指的就是动态开辟的数组
+但数组长度还是在创建时指定
+数组长度并不会在生命周期内发生变化
+
+
+*/
 int LocateElem(int* oneDimensionalArray, int elemVal, int arraySize);
 int GetElem(int* oneDimensionalArray, int elemLoc, int arraySize);
 void InsertElem(int* oneDimensionalArray, int elemLoc, int elemVal);
